c6b68ebdab62115a587d0339fa1f1c5ca90d837d
tfobadar <89089416+tfobadar@users.noreply.github.com>
2024-02-26 22:43:21 +0300

    Button and Text Widgets styling

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'package:advance_basic/pages/quiz.dart';
+import 'package:advance_basic/pages/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ pages/question_screen.dart
@@ -0,0 +1,48 @@
+import 'package:advance_basic/components/answer_button.dart';
+import 'package:advance_basic/components/heading2_text.dart';
+import 'package:advance_basic/data/questions.dart';
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuestionScreenState createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void nextQuestion() {
+    setState(() {
+      if (currentQuestionIndex < 6) {
+        currentQuestionIndex++;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Heading2Text(
+              title: currentQuestion.text, textAlignment: TextAlign.center),
+          const SizedBox(
+            height: 20,
+          ),
+          ...currentQuestion.getShuffledAnswers().map((item) {
+            return AnswerButton(
+              answer: item,
+              onTap: nextQuestion,
+            );
+          })
+        ],
+      ),
+    );
+  }
+}
